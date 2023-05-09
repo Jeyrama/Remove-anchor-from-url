aaa3d5db965d10147eb46afd9fe702d74aad2553
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-09 08:41:07 -0700

    Create Remove-anchor-from-url.js

---
+++ Remove-anchor-from-url.js
@@ -0,0 +1,21 @@
+/*
+Complete the function/method so that it returns the 
+url with anything after the anchor (#) removed. 
+
+Examples:
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+*/
+
+
+// Solution
+
+function removeUrlAnchor(url) {
+  return url.split('#')[0];
+}
+
+// or
+
+function removeUrlAnchor(url) {
+  return url.replace(/#.*/gi,"");
+}
